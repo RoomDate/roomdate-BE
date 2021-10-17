6af32be7ad7801aeb70c0686fc8c9a48a0223f04
Cristian Montes <cmontesmujica@gmail.com>
2021-10-17 15:23:56 -0700

    seed data

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,11 +2,32 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+const seedUsernames = require('../data/seedUsernames.js');
 
 describe('roomdate routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('hshshs', async () => {
+    expect(true).toEqual(true);
+  });
+
+
+  //___________----------------------------------------------------------------------------------//
+
+  it('SEED users_main', async () => {
+    const usersMainData = await Promise.all(seedUsernames.map(async (username) =>  await pool.query(`
+        INSERT INTO users_main ( google_id, username) 
+        VALUES($1, $2) RETURNING *`, [username.google_id, username.username])));
+
+    expect(usersMainData).toEqual(seedUsernames);
+  });
+  
+  //___________----------------------------------------------------------------------------------//
+
+
+
 
   afterAll(() => {
     pool.end();
--- sql/setup.sql
+++ sql/setup.sql
@@ -9,7 +9,7 @@ DROP TABLE IF EXISTS users_profile CASCADE;
 
 
 CREATE TABLE users_main (
-  	gooogle_id TEXT NOT NULL UNIQUE, 
+  	google_id TEXT NOT NULL UNIQUE, 
     username TEXT NOT NULL PRIMARY KEY
 );
 
@@ -32,6 +32,7 @@ CREATE TABLE roles (
 CREATE TABLE users_info (
 	user_info_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   	first_name TEXT NOT NULL,
+    username TEXT NOT NULL REFERENCES users_main(username),
   	last_name TEXT NOT NULL,
   	dob DATE NOT NULL,
   	gender TEXT NOT NULL,
@@ -50,6 +51,7 @@ CREATE TABLE preferences(
 	preference_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   	username TEXT NOT NULL REFERENCES users_main(username),
 	smoke BOOLEAN,
+    gender TEXT,
   	drugs BOOLEAN,
   	alcohol BOOLEAN,
   	introvert BOOLEAN,
@@ -66,12 +68,12 @@ CREATE TABLE preferences(
 
 CREATE TABLE users_profile (
   	user_profile_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  	preference_id BIGINT NOT NULL,
-  	username TEXT NOT NULL,
-  	role_type BIGINT NOT NULL,
-  	employment_id BIGINT NOT NULL, 
-  	education_id BIGINT NOT NULL,
-  	user_info BIGINT NOT NULL
+  	preference_username BIGINT NOT NULL REFERENCES preferences(preference_id),
+  	username TEXT NOT NULL REFERENCES users_main(username),
+  	role_type BIGINT NOT NULL REFERENCES roles(role_id),
+  	employment_id BIGINT NOT NULL REFERENCES employment(employment_id),
+  	education_id BIGINT NOT NULL REFERENCES  education(education_id),
+  	user_info BIGINT NOT NULL REFERENCES users_info(user_info_id)
 --   	matches TEXT[],
 --   	liked_profiles TEXT [],
 --   	disliked_profiles TEXT []
