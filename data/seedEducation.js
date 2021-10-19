6fb60aff6cc73ec443933b21e14345e9e6b002fe
maddenlockin <maddenlockin@gmail.com>
2021-10-19 15:29:09 -0700

    merge conflicts and logout route in controller

--- data/seedEducation.js
+++ data/seedEducation.js
@@ -1,14 +1,14 @@
 module.exports = [
-  {
-    education_id: 1,
-    education_status: 'in college'
-  },
-  {
-    education_id: 2,
-    education_status: 'graduate'
-  },
-  {
-    education_id: 3,
-    education_status: 'not in school'
-  }
+    {
+        education_id: 1,
+        education_status: 'in college'
+    },
+    {
+        education_id: 2,
+        education_status: 'graduate'
+    },
+    {
+        education_id: 3,
+        education_status: 'not in school'
+    }
 ];
